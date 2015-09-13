051e1d5648723138c5f7d3593ae1805304f76582
Chris McCammon <cmccammon@gmail.com>
2015-09-12 21:32:17 -0500

    finally making progress

---
+++ bin/request.rb
@@ -0,0 +1,6 @@
+require_relative '../db/setup'
+require_relative '../lib/all'
+
+get_name = Name.new
+
+get_name
---
+++ lib/name.rb
@@ -0,0 +1,21 @@
+require_relative 'todo'
+
+
+class Name
+  nm = Todo.new
+
+  puts "What is your name? >"
+  response = gets.chomp
+  nm.name = response
+
+  puts "what task would you like to complete? >"
+  tsk = gets.chomp
+  nm.task = tsk
+
+  nm.save
+
+  puts "your info is name: #{nm.name} task: #{nm.task} completed: #{nm.completed}"
+
+
+end
+
